13ca7480692faacc1d8fec216720ed4f2750fe49
Adrian Wisniewski <sirizaak@gmail.com>
2015-10-17 20:45:34 -0700

    Converted tabs to spaces.

--- square/main.cpp
+++ square/main.cpp
@@ -1,31 +1,31 @@
 #include "stdafx.h"
 
 namespace {
-	const int SCREEN_WIDTH = 640;
-	const int SCREEN_HEIGHT = 480;
-	const int GAME_FRAME_MS = 10;
+  const int SCREEN_WIDTH = 640;
+  const int SCREEN_HEIGHT = 480;
+  const int GAME_FRAME_MS = 10;
 }
 
 struct SDL {
 public:
-	~SDL() {
-		SDL_Quit();
-	}
+  ~SDL() {
+    SDL_Quit();
+  }
 };
 
 struct SDL_WindowDeleter
 {
-	void operator()(SDL_Window *window) const
-	{
-		SDL_DestroyWindow(window);
-	}
+  void operator()(SDL_Window *window) const
+  {
+    SDL_DestroyWindow(window);
+  }
 };
 
 struct SDL_GLContextDeleter {
-	void operator()(SDL_GLContext glContext) const
-	{
-		SDL_GL_DeleteContext(glContext);
-	}
+  void operator()(SDL_GLContext glContext) const
+  {
+    SDL_GL_DeleteContext(glContext);
+  }
 };
 
 
@@ -58,38 +58,38 @@ int main( int argc, char* args[] )
 
   glOrtho(0.0, SCREEN_WIDTH, 0.0, SCREEN_HEIGHT, -1.0, 1.0);
 
-	float x = 0.0f;
-	float y = 0.0f;
+  float x = 0.0f;
+  float y = 0.0f;
 
-	unsigned lastSimulationTimeMs = SDL_GetTicks();
-	while (true) {
-		unsigned now = SDL_GetTicks();
+  unsigned lastSimulationTimeMs = SDL_GetTicks();
+  while (true) {
+    unsigned now = SDL_GetTicks();
 
-		SDL_Event e;
-		while (SDL_PollEvent(&e) != 0)
-		{
-			if (e.type == SDL_QUIT)
-			{
-				return 0;
-			}
-		}
+    SDL_Event e;
+    while (SDL_PollEvent(&e) != 0)
+    {
+      if (e.type == SDL_QUIT)
+      {
+        return 0;
+      }
+    }
 
-		if (now - lastSimulationTimeMs < GAME_FRAME_MS) {
-			SDL_WaitEventTimeout(NULL, now - lastSimulationTimeMs);
-			continue;
-		}
+    if (now - lastSimulationTimeMs < GAME_FRAME_MS) {
+      SDL_WaitEventTimeout(NULL, now - lastSimulationTimeMs);
+      continue;
+    }
 
-		while (now - lastSimulationTimeMs >= GAME_FRAME_MS) {
-			lastSimulationTimeMs += GAME_FRAME_MS;
+    while (now - lastSimulationTimeMs >= GAME_FRAME_MS) {
+      lastSimulationTimeMs += GAME_FRAME_MS;
 
-			x += 1.0f;
-			y += 1.0f;
-		}
+      x += 1.0f;
+      y += 1.0f;
+    }
 
-		glClear(GL_COLOR_BUFFER_BIT);
-		glColor3f(1.0f, 1.0f, 1.0f);
-		glRectf(x, y, x + 100.0f, y + 100.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glRectf(x, y, x + 100.0f, y + 100.0f);
 
-		SDL_GL_SwapWindow(window.get());
-	}
+    SDL_GL_SwapWindow(window.get());
+  }
 }
