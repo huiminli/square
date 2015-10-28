cd66dd742f0c092a2be93231d7519a39db71c782
Adrian Wisniewski <adr.wisniewski@gmail.com>
2015-10-27 23:42:59 -0700

    Optimized vertex shader. Removed texture coords buffer.

--- square/vertex.glsl
+++ square/vertex.glsl
@@ -1,19 +1,21 @@
 #version 410 core
 
 uniform vec2 worldPosition;
-uniform vec2 tileTextureUV;
-
+uniform uint tileIndex;
 layout(location = 0) in vec2 vPosition;
-layout(location = 1) in vec2 vTextureUV;
-
 out vec2 fTextureUV;
 
-mat4 projectionMatrix = mat4(2.0/800.0, 0.0,       0.0, -1.0,
-                             0.0,       2.0/640.0, 0.0, -1.0,
-                             0.0,       0.0,       1.0,  0.0,
-                             0.0,       0.0,       0.0,  1.0);  
+const vec2 tileUV = vec2(1.0 / 16, 1.0 / 16);
+const mat4 projectionMatrix = mat4(
+  2.0/800.0, 0.0,       0.0, -1.0,
+  0.0,       2.0/640.0, 0.0, -1.0,
+  0.0,       0.0,       1.0,  0.0,
+  0.0,       0.0,       0.0,  1.0);  
 
 void main(){
-  gl_Position = vec4(worldPosition + vPosition, 0.0, 1.0) * projectionMatrix;
-  fTextureUV = vTextureUV + tileTextureUV;
+  gl_Position = vec4(worldPosition + vPosition * 32, 0.0, 1.0) * projectionMatrix;
+
+  vec2 tileIndex2D = vec2(mod(tileIndex, 16), tileIndex / 16);
+  vec2 tileQuadOffset = vec2(vPosition.x, 1 - vPosition.y);
+  fTextureUV = tileUV * (tileQuadOffset + tileIndex2D);
 }
